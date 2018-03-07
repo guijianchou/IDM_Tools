38e40695308074ce9b960d839ed0555047687140
guijianchou <falsemeet@gmial.com>
2018-03-07 03:08:12 -0800

    redesign some code and class

--- copyfile.py
+++ copyfile.py
@@ -11,16 +11,14 @@ filelist=[]
 rootdir="/mnt/c/Users/falsemeet/Downloads/"
 fileDst="/mnt/f/Downloads"
 filelist=os.listdir(rootdir)
-def __copy_file1():
+def copy_file1():
     for x in filelist:
         print x
         m=os.path.join(rootdir,x)
         print m
         shutil.copy2(m,fileDst)
         os.remove(m)
-def copy_file2():
-    __copy_file1()
-    print "copy and remove finished !\n"
 
 if __name__=='__main__':
-    copy_file2()
+    copy_file1()
+    print "copy and remove finished !"
