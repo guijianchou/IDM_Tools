aa585ccf845f9fc653fa5ef7101c5f658750e3d2
GuiJianChou <guijianchou@users.noreply.github.com>
2020-05-16 19:53:20 -0700

    Update copyfile.py

--- copyfile.py
+++ copyfile.py
@@ -8,7 +8,7 @@ __auther__='guijianchou'
 import os
 import shutil
 filelist=[]
-rootdir="/mnt/c/Users/falsemeet/Downloads/"
+rootdir="/mnt/c/Users/name/Downloads/"
 fileDst="/mnt/f/Downloads"
 filelist=os.listdir(rootdir)
 def copy_file1():
