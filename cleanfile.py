d9df17d70c2f4b20789280a26735248a5b24651a
Guijianchou <falsemeet@gmail.com>
2021-04-10 02:23:57 -0700

    move to py3

--- cleanfile.py
+++ cleanfile.py
@@ -8,25 +8,25 @@ __auther__='guijianchou'
 import os
 import shutil
 filelist=[]
-print 'If your have different TEMP path, Please change your TEMP path : \n'
+print("If your have different TEMP path, Please change your TEMP path : \n")
 #filepath=input()
 filepath="/mnt/e/Applications/Idm/Temps/DwnlData" #if you use the defult or self folder, Please Change the correct Path. for some reason, win cannot get os.uname(),some codes hard to change
 filelist=os.listdir(filepath)
 def __clean_file1():
     for x in filelist:
-        print x
+        print (x)
         m=os.path.join(filepath,x)
-        print m
+        print (m)
         try:
             shutil.rmtree(m)
         except TypeError,e:
-            print 'Exception:',e
+            print ('Exception:',e)
         finally:
             pass
     
 def clean_file2():
     __clean_file1()  
-    print 'already clean the \'IDM TEMPS\' !\n'
+    print ('already clean the \'IDM TEMPS\' !\n')
 
 if __name__=='__main__':
     clean_file2()
--- hash.py
+++ hash.py
@@ -11,26 +11,24 @@ import sys
 
 
 
-filelist="/mnt/f/Download"
-hashname="/mnt/f/Download/hash.txt"
+filelist=os.listdir("/mnt/e/Downloads")
+#hashname=os.path("/mnt/e/Downloads/hash.txt")
 md5=hashlib.md5()
 sha1=hashlib.sha1()
 def __hashdownload():
     for x in filelist:
-        print x
-        md5_x=md5(x)
-        sha1_x=sha1(x)
-
-        m=md5_x.hexdigest(x)
-        n=sha1_x.hexdigest(x)
+        print(os.fspath(x))
+        m=md5.update(x.encode('utf-8'))
+        print(m)
+    #    n=sha1.update(x.encode('utf-8')).hexdigest()
         
-        with open(hashname,'w') as t:
-            t.write(x,'\n','md5:',m,'\n','sha1:',n,'\n\n')
-            t.close()
-        print x,' md5:',m
+#        with open(hashname,'w') as t:
+ #           t.write(x,'\n','md5:',m,'\n','sha1:',n,'\n\n')
+ #           t.close()
+ #       print (x,' md5:',m)
 def hashfile():
     __hashdownload()
-    print "already add new file hash into hash.txt"
+    print ("already add new file hash into hash.txt")
 
 if __name__=='__main__':
     hashfile()
